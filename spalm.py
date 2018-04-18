6776f3ce4a3a0ad45e024721d3385635bf64a799
Pedro Borges Costa <pedrobc89@gmail.com>
2018-04-18 18:16:50 -0300

    matching complete

--- spalm.py
+++ spalm.py
@@ -1,26 +1,68 @@
 import csv
+import re
+from time import sleep
+from unicodedata import normalize
+
+def remover_acentos(txt):
+    return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
+
+def find_keywords(text):
+    rv = regex.sub(' ', remover_acentos(text)).lower().split()
+    rv = set(rv)
+    rv = list(rv)
+    return rv
+    
 
 spalm = []
-livraria = []
+livraria = {}
 codigo_spalm = []
+regex_out = ["a", "as", "na", "nas", "o", "os", "no", "nos", "e"]
+regex_pattern = "[^A-Za-z0-9]+"
+for s in regex_out:
+    regex_pattern += (r"|\b{}\b".format(s))
+regex = re.compile(regex_pattern, flags=re.IGNORECASE)
+
+def match_string(s, kw1):
+
+    kw2 = find_keywords(s)
+    rv = 0
+    for kw in kw1:
+        rv += kw2.count(kw)
+    return rv
+
+def read_csv(filename):
+    print('Reading', filename)
+    with open(filename, 'r') as csvfile:
+        rows = csv.reader(csvfile, delimiter=';')
+        #  Skips header
+        next(rows, None)
+        for row in rows:
+            yield row
+            #  return
+
+for row in read_csv('spalm.csv'):
+    title = row[0]
+    number = row[1]
+    spalm.append([title, number])
 
 
-def match_string(s1, s2):
-    """TODO: Docstring for match_string.
-    :returns: TODO
+for row in read_csv('livraria.csv'):
+    title = row[0]
+    kw = find_keywords(title)
+    livraria[title] = kw
+    print(title)
+    print(kw)
+    matches = []
+    for spalm_title, num in spalm:
+        match = match_string(spalm_title, kw)
+        if match > 1:
+            v = [match, spalm_title, num]
+            matches.append(v) 
+            #  print(v, len(matches))
+            #  sleep(5)
+    matches = sorted(matches, key=lambda s :s[0], reverse=True)
+    for i, row in enumerate(matches):
+        print("{:0=2d} Score:{}, Título: {}, Código: {}".format(i, *row))
+    break
 
-    """
-    ss = s1.split()
-    return 0
-
-with open('teste.csv', 'r') as csvfile:
-    creader = csv.reader(csvfile, delimiter=';')
-    for name, num in creader:
-        print(name)
-
-
-with open('teste2.csv', 'r') as csvfile:
-    creader = csv.reader(csvfile, delimiter=';')
-    for name in creader:
-        print(name)
-
+    #  spalm.append([title, number])
